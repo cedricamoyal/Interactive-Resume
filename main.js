0b046703cc24ff28375b5d9e33a4fc3fbb08305c
cedricamoyal <cedric.amoyal@gmail.com>
2016-07-03 11:21:26 +1000

    with who am I

--- main.js
+++ main.js
@@ -24,27 +24,27 @@
 		var dataset1 = {
 			nodes:[
 					{name1:"Who",name2:"am I?", color: "#FAF9F4", radius: 90,
-              description: 'I am a French-Australian web developer who became passionate by web development lately. The 12 weeks fulltime course that I just did with General Assembly confirmed everything I thought: I am willing to become a web developer.<br>Why did I decide to become a web developer?<br>I have been a civil engineer/project manager for the past 8 years. Looking for new challenges, I started to get involved in the start-up community. I even decided to start my own business: Value Your Time (www.valueyourtime.com.au).<br>But after few months I realised that I enjoy developing web solutions much more than every other aspect of the business. I heard about Web Development Bootcamp and decided to go to General Assembly Web Development Immersive Program.<br>That experience confirmed everything I thought: I am willing to become a web developer.<br>You can see below the links to the pdf version of my resume and to my linkedin:<br><a href="CedricAmoyal2016WD.pdf" target="_blank"><span><i class="fa fa-file-text-o" aria-hidden="true"></i></span></a> <a href="https://au.linkedin.com/in/cedric-amoyal-83b5a747" target="_blank"><span><i class="fa fa-linkedin-square" aria-hidden="true"></i></span></a>'},
+              description: 'I am a French-Australian web developer who became passionate by web development lately.<br><br>I have been a civil engineer/project manager for the past 8 years (the last 5 years in Australia). I have been part of the construction team on various types of projects such as wind farm, power plant, dam and building.<br><br>Looking for new challenges I decided to get involved in the start-up community. I even decided to start my own business: Value Your Time.<br><br>But after few months I realised that I enjoy developing web solutions much more than every other aspect of the business.<br><br>The 12 weeks fulltime course that I just did with General Assembly confirmed everything I thought: I am willing to become a web developer.<br><br>You can see below the links to the pdf version of my resume, to my linkedin and to Value Your Time website:<br><br><a class="whoAmI" href="CedricAmoyal2016WD.pdf" target="_blank"><span><i class="fa fa-file-text-o" aria-hidden="true"></i></span></a> <a class="whoAmI" href="https://au.linkedin.com/in/cedric-amoyal-83b5a747" target="_blank"><span><i class="fa fa-linkedin-square" aria-hidden="true"></i></span></a><a class="whoAmI" href="http://www.valueyourtime.com.au/" target="_blank"><span><i class="fa fa-hourglass" aria-hidden="true"></i></span></a>'},
 
-	        {name1:"Ex Civil",name2: "Engineer", color: "#52B4AF", radius: 70,
+	        {name1:"Previous Life:",name2: "Civil Engineer", color: "#52B4AF", radius: 70,
               description: "#"},
-					{name1:"New Web",name2:"Developer", color: "rgb(183, 178, 224)", radius: 70,
+					{name1:"New Life:",name2:"Web Developer", color: "rgb(183, 178, 224)", radius: 70,
               description: 'What did I learn during the WDi program?<br>The WDi Program taught me the skills I need to kickstart my career as a web developer.<br>From programming fundamentals (HTML, CSS, JavaScript, jQuery, AJAX, Ruby) to launching full-stack web apps (Rails, Backbone.js, Bootstrap), I learned to solve problems with code while applying industry best practices in a collaborative environment  (Github, Heroku).'},
 
-					{name1:"Project",name2:"Manager", color: "#B6DEDC", radius: 55,
+					{name1:"Positions",name2:"& Projects", color: "#B6DEDC", radius: 55,
               description: "#"},
-					{name1:"Project",name2:"Engineer", color: "#B6DEDC", radius: 55,
+					{name1:"Main",name2:"Activities", color: "#B6DEDC", radius: 55,
               description: "#"},
-          {name1:"Site",name2:"Engineer", color: "#B6DEDC", radius: 55,
+          {name1:"Regular",name2:"Duties", color: "#B6DEDC", radius: 55,
               description: "#"},
 
-          {name1:"Project 4",name2:"Final", color: "rgb(203, 170, 193)", radius: 55,
+          {name1:"A Mobile App",name2:"with React", color: "rgb(203, 170, 193)", radius: 55,
               description: 'Link to the application:<br><a class="webDevAtag" href="#" target="_blank">#</a> <br>Link to the code:<br><a class="webDevAtag" href="#" target="_blank">#</a>'},
-          {name1:"Twitter Data",name2:"Visualisation", color: "rgb(203, 170, 193)", radius: 55,
+          {name1:"Visualisation",name2:"with d3.js", color: "rgb(203, 170, 193)", radius: 55,
               description: 'Link to the application:<a class="webDevAtag" href="https://project3group.herokuapp.com/" target="_blank"><span class="devicons devicons-heroku"></span></a> <br>Link to the code:<a class="webDevAtag" href="https://github.com/cedricamoyal/project3group " target="_blank"><span class="devicons devicons-github_badge"></span></a>'},
-          {name1:"Travel",name2:"The World", color: "rgb(203, 170, 193)", radius: 55,
+          {name1:"A CRUD App",name2:"with Rails", color: "rgb(203, 170, 193)", radius: 55,
               description: 'Link to the application:<a class="webDevAtag" href="https://cedricproject2.herokuapp.com/login " target="_blank"><span class="devicons devicons-heroku"></span></a> <br>Link to the code:<a class="webDevAtag" href="https://github.com/cedricamoyal/project2cedricheroku " target="_blank"><span class="devicons devicons-github_badge"></a>'},
-          {name1:"Tic",name2:"Tac Toe", color: "rgb(203, 170, 193)", radius: 55,
+          {name1:"A Little Game",name2:"with jQuery", color: "rgb(203, 170, 193)", radius: 55,
               description: 'Link to the application:<a class="webDevAtag" href="http://cedricamoyal.github.io/Cedricwebsite " target="_blank"><span class="devicons devicons-github_alt"></span></a> <br>Link to the code:<a class="webDevAtag" href="https://github.com/cedricamoyal/Cedricwebsite " target="_blank"><span class="devicons devicons-github_badge"></span></a>'},
 
 			],
